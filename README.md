e81d312d9b863eb66760abe2208cee10ca3702f3
JacobKnaack <jacobknck@gmail.com>
2022-02-04 14:01:34 -0800

    updated readme and adds error handling

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # invokeMe
+
 Function deployed to AWS Lambda
+
+## Usage
+
+Add credentials for the following github action secrets:
+
+* AWS_ACCESS_KEY_ID
+* AWS_SECRET_ACCESS_KEY
+* AWS_REGION
+
+Update the `function_name` to include your own AWS function under the account associated with the above credentials.
+
+Make sure you user role is authorized to access AWS Lambda Services.
--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const friendModel = require('./friend.model.js');
 exports.handler = async (event) => {
   
   console.log(event);
+  const response = {statusCode: null, body: null}
 
-  let { id, name, phone } = event;
-  
-  let newFriend = await friendModel.create({ id, name, phone });
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(newFriend),
-  };
+  try {
+    let { id, name, phone } = event;
+    
+    let friendRecord = await friendModel.create({ id, name, phone });
+    response.statusCode = 200;
+    response.body = JSON.stringify(friendRecord);
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({Error: 'invalid payload'});
+  }
   return response;
 };
